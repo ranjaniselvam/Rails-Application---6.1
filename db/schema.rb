d8fad8366fd4b81c3458414673937df5aeaef536
Sivaranjani Selvam <sivaranjani@mallow-tech.com>
2023-04-07 14:23:41 +0530

    topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,4 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2023_04_07_084425) do
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
